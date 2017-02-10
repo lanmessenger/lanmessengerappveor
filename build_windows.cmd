9e40886ef91df6bb14226fcfb113895e377e2cd8
lanmessenger <lanmsgnr@gmail.com>
2017-02-10 15:58:50 +0300

    1

--- build_windows.cmd
+++ build_windows.cmd
@@ -4,7 +4,7 @@ SET project_dir="%cd%"
 
 echo Set up environment...
 set PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;%PATH%
-call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" %PLATFORM%
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
 
 echo Building lmcapp...
 cd ./lmcapp/src
