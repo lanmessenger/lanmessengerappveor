f91ea65443608b6fe453092597276bd137eec7a9
lanmessenger <lanmsgnr@gmail.com>
2017-02-10 12:21:38 +0300

    1

---
+++ build_windows.cmd
@@ -0,0 +1,37 @@
+echo on
+
+SET project_dir="%cd%"
+
+echo Set up environment...
+set PATH=%QT%\bin\;C:\Qt\Tools\QtCreator\bin\;C:\Qt\QtIFW2.0.1\bin\;%PATH%
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %PLATFORM%
+
+echo Building YourApp...
+qmake -spec win32-msvc2015 CONFIG+=x86_64 CONFIG-=debug CONFIG+=release
+nmake
+
+::echo Running tests...
+
+::echo Packaging...
+::cd %project_dir%\build\windows\msvc\x86_64\release\
+::windeployqt --qmldir ..\..\..\..\..\src\ YourApp\YourApp.exe
+
+::rd /s /q YourApp\moc\
+::rd /s /q YourApp\obj\
+::rd /s /q YourApp\qrc\
+
+::echo Copying project files for archival...
+::copy "%project_dir%\README.md" "YourApp\README.md"
+::copy "%project_dir%\LICENSE" "YourApp\LICENSE.txt"
+::copy "%project_dir%\Qt License" "YourApp\Qt License.txt"
+
+::echo Copying files for installer...
+::mkdir "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data\"
+::robocopy YourApp\ "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data" /E
+
+::echo Packaging portable archive...
+::7z a YourApp_%TAG_NAME%_windows_x86_64_portable.zip YourApp
+
+::echo Creating installer...
+::cd %project_dir%\installer\windows\x86_64\
+::binarycreator.exe --offline-only -c config\config.xml -p packages YourApp_%TAG_NAME%_windows_x86_64_installer.exe
