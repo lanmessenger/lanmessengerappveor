24fe5f241587d904796254a518ca5a58a9fe9a49
lanmessenger <lanmsgnr@gmail.com>
2017-02-21 18:19:54 +0300

    6

--- build_windows.bat
+++ build_windows.bat
@@ -54,29 +54,3 @@ goto endmake
 
 
 :endmake
-
-::echo Running tests...
-
-::echo Packaging...
-::cd %project_dir%\build\windows\msvc\x86_64\release\
-::windeployqt --qmldir ..\..\..\..\..\src\ YourApp\YourApp.exe
-
-::rd /s /q YourApp\moc\
-::rd /s /q YourApp\obj\
-::rd /s /q YourApp\qrc\
-
-::echo Copying project files for archival...
-::copy "%project_dir%\README.md" "YourApp\README.md"
-::copy "%project_dir%\LICENSE" "YourApp\LICENSE.txt"
-::copy "%project_dir%\Qt License" "YourApp\Qt License.txt"
-
-::echo Copying files for installer...
-::mkdir "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data\"
-::robocopy YourApp\ "%project_dir%\installer\windows\x86_64\packages\com.yourappproject.yourapp\data" /E
-
-::echo Packaging portable archive...
-::7z a YourApp_%TAG_NAME%_windows_x86_64_portable.zip YourApp
-
-::echo Creating installer...
-::cd %project_dir%\installer\windows\x86_64\
-::binarycreator.exe --offline-only -c config\config.xml -p packages YourApp_%TAG_NAME%_windows_x86_64_installer.exe
